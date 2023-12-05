9c06c16eea75adb5e3d9e3d70a8e4800a8a64f23
KURT BANWELL PACHERNEGG <KURT.BANWELL.PACHERNEGG@HOMEAFFAIRS.GOV.AU>
2023-12-05 13:16:30 +1100

    add excess ingredient bounds

--- satispy.py
+++ satispy.py
@@ -7,7 +7,7 @@ if not os.path.exists("results"):
     os.makedirs("results")
     
 def create_summary(name : str, summary : dict):
-    with open('results/' + name + '-summary.yaml', 'w') as file:
+    with open('results/' + name + '.yaml', 'w') as file:
         yaml.dump(summary, file, sort_keys=False)
 
 # The following are some examples of how to use the optimize functions.
@@ -18,11 +18,20 @@ summary = optimize_points()
 create_summary('points', summary)
 
 # ------------------------------------------------------------------------------
-# I'm looking to make an optimized set of recipies for PPM without creating
-# any waste. So I've added two ways to manage this. You can now disable any
-# recipe you don't want to use, and you can also set any item to be unsinkable.
-# This will make sure that the item is always 0 in the solution (i.e. no excess,
-# the item must be produced as much as it is consumed by recipes).
+# Optimize for the most efficient way to produce power.
+
+summary = optimize_power()
+create_summary('power', summary)
+
+# ------------------------------------------------------------------------------
+# Optimize for making the most Thermal Propulsion Rockets per minute.
+
+summary = optimize_item('THERMAL_PROPULSION_ROCKET')
+create_summary('thermal-propulsion-rocket', summary)
+
+# ------------------------------------------------------------------------------
+# Optimize for making the most points without creating unsinkable waste
+
 summary = optimize_points(
     disable_recipes = ['PLUTONIUM_POWER'],
     disable_items = [],
@@ -49,13 +58,36 @@ summary = optimize_points(
 create_summary('sustainable-points', summary)
 
 # ------------------------------------------------------------------------------
-# Optimize for the most efficient way to produce power.
+# For my own map, I'm looking to make an optimized set of recipies for PPM 
+# without creating any waste.
+# I also need some spare power for trains and batteries for drones so that the
+# map is fun.
 
-summary = optimize_power()
-create_summary('power', summary)
-
-# ------------------------------------------------------------------------------
-# Optimize for making the most Thermal Propulsion Rockets per minute.
-
-summary = optimize_item('THERMAL_PROPULSION_ROCKET')
-create_summary('thermal-propulsion-rocket', summary)
+summary = optimize_points(
+    disable_recipes = ['PLUTONIUM_POWER'],
+    disable_items = [],
+    unsinkable_items = [
+        
+        'WATER',
+        'CRUDE_OIL',
+        'HEAVY_OIL_RESIDUE',
+        'FUEL',
+        'TURBOFUEL',
+        'ALUMINA_SOLUTION',
+        'SULFURIC_ACID',
+        'NITRIC_ACID',
+        'NITROGEN_GAS',
+        
+        'URANIUM_WASTE', 
+        'PLUTONIUM_WASTE',
+        'NON_FISSILE_URANIUM',
+        'PLUTONIUM_PELLET',
+        'ENCASED_PLUTONIUM_CELL',
+        
+    ],
+    ingredient_bounds = {
+        "POWER": (60 * 20000, None),
+        "BATTERY": (100, )
+    }
+)
+create_summary('custom', summary)
